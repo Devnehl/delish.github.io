f53dff893f9e9d50e7f097029d8ef2285b574275
nehldotjs <nelsonosuya11@gmail.com>
2023-02-27 14:21:13 +0100

    NEW COMMIT

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div>
+      <h3> Mssg from DELISH</h3>
+      <Home />
+    </div>
+  );
+}
+export default App;
